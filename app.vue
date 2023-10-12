517e40d472c48a6ab779879078c292581bfa6b24
Panat Bangsang-oon <panat@owndays-tech.com>
2023-10-12 17:38:38 +0700

    first-commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout> -->
+  <h1 class="text-3xl font-bold underline text-center">
+    Hello world!
+  </h1>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'My GPT',
+  meta: [
+    { name: 'Open AI', content: 'My amazing GPT' }
+  ]
+})
+</script>
