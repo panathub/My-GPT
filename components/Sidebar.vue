30e07de2d16a070707e9e8e52693bde09107ccbe
Panat Bangsang-oon <panat@owndays-tech.com>
2023-10-17 19:05:19 +0700

    update style

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,31 +1,34 @@
 <template>
-
-<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-   <span class="sr-only">Open sidebar</span>
-   <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-   </svg>
-</button>
-
-<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div class="h-full w-[260px] px-2 py-2 overflow-y-auto bg-[#202123]">
-         <div class="flex gap-2 text-white text-sm">
-            <div class="items-center p-3 rounded-lg border border-gray-500 w-full">
-                <Icon name="ep:plus" color="white" />
-               <span class="ml-3">New Chat</span>
+<div>
+   <aside id="default-sidebar" 
+      class="overflow-hidden top-0 left-0 z-40 h-screen transition-all duration-300"
+      :class="close ? 'w-0' : 'w-[260px]'"
+      >
+      <div class="w-[260px] h-full px-2 py-2 overflow-y-auto bg-[#202123]">
+            <div class="flex gap-2 text-white text-sm">
+               <div class="items-center p-3 rounded-lg border border-gray-500 w-full">
+                  <Icon name="ep:plus" color="white" />
+                  <span class="ml-3">New Chat</span>
+               </div>
+               <button @click="closeSideBar" class="items-center p-3 rounded-lg border border-gray-500">
+                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="icon-sm text-token-text-primary gizmo:text-token-text-tertiary gizmo:hover:text-token-text-secondary" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="3" x2="9" y2="21"></line></svg>
+               </button>
             </div>
-            <div class="items-center p-3 rounded-lg border border-gray-500">
-                <Icon name="ep:close" color="white" />
+            <div class="flex-col flex-1 overflow-y-auto border-b border-white/20">
             </div>
-         </div>
-   </div>
-</aside>
+            <div>
+            </div>
+      </div>
+   </aside>
+</div>
 
 
 </template>
 
-<script setup lang="ts">
+<script setup>
 
+const props = defineProps(['close', 'closeSideBar'])
+console.log(props.close)
 </script>
 
 <style lang="sass" scoped>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
  
-  <div class="w-screen h-screen flex overflow-hidden">
-    <Sidebar/>
-    <slot />
+  <div class="w-screen h-screen overflow-hidden flex">
+    <section>
+      <Sidebar :close=close :closeSideBar=closeSideBar />
+    </section>
+    <section class="flex-1">
+        <Navbar :close=close :closeSideBar=closeSideBar />
+        <slot />
+      </section>
   </div>
+  <Modal />
 </template>
+
+<script setup>
+const close = ref(false)
+
+function closeSideBar() {
+   close.value = !close.value
+   console.log('click', close.value)
+}
+</script>
+
